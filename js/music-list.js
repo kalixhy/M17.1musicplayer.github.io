eb35dcbe1f30a451c487b5b61585be9f111845c3
kalixhy <126723717+kalixhy@users.noreply.github.com>
2023-11-14 06:21:15 +0300

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,179 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Rockstar",
+    artist: "DaBaby ft Roddy Ricch",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Goodbyes",
+    artist: "Postmalone",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Save your tears",
+    artist: "The weekend",
+    img: "music-3",
+    src: "music-3"
+    
+  },
+  {
+    name: "Blinding lights",
+    artist: "The weekend",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Black and white",
+    artist: "Juice wrld",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Stay High",
+    artist: "Juice wrld",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Righteous",
+    artist: "Juice wrld",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "lean wit me",
+    artist: "Juice wrld",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Robbery",
+    artist: "Juice wrld",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Ferrari",
+    artist: "Pop Smoke",
+    img: "music-10",
+    src: "music-10"
+  }, 
+  {
+    name: "Dior",
+    artist: "Pop Smoke",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Dubai",
+    artist: "Krills",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "6 am",
+    artist: "Arrdde",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "Oliver twist",
+    artist: "Arrdde",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "Hello Mate",
+    artist: "Arrdde",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "Flowers",
+    artist: "Arrdde",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "Come and Go",
+    artist: "Arrdde",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "Doja",
+    artist: "Central cee",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "Obssesed",
+    artist: "Central cee",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "6 for 6",
+    artist: "Central cee",
+    img: "music-20",
+    src: "music-20"
+  },
+  {
+    name: "Sprinter",
+    artist: "Central cee ft Dave",
+    img: "music-21",
+    src: "music-21"
+  },
+  {
+    name: "On and On",
+    artist: "Cartoon",
+    img: "music-22",
+    src: "music-22"
+  },
+  {
+    name: "Let me love you",
+    artist: "Dj snake ft Justin beiber",
+    img: "music-23",
+    src: "music-23"
+  },
+  {
+    name: "Diamond Heart",
+    artist: "Alan walker ft Sophia Somajo",
+    img: "music-26",
+    src: "music-26"
+  },
+  {
+    name: "Darkside",
+    artist: "Alan walker ",
+    img: "music-26",
+    src: "music-26"
+  },
+  {
+    name: "Hero",
+    artist: "Alan walker ft Sasha Alex",
+    img: "music-26",
+    src: "music-26"
+  },
+
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
